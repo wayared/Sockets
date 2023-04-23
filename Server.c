98fcd139eb6c176af0009ad64f8653b6034057cc
wayared <jarecast@espol.edu.ec>
2023-04-22 20:09:19 -0700

    Server main function

--- Server.c
+++ Server.c
@@ -33,6 +33,16 @@ int main(int argc, char *argv[] ){
 	}
 
 	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = atoi(argv[1]);
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+
+	if(bind(sockfd , (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
+		error("Binding Failed.");
+	}
+
 
 
 
