d5c9c687bb6776b8e6b6ac33f40a39c774219210
wayared <jarecast@espol.edu.ec>
2023-04-23 21:44:28 -0700

    Client.c main function

--- Client.c
+++ Client.c
@@ -35,8 +35,17 @@ int main(int argc, char *argv[]){
 	sockfd = socket(AF_INET, SOCK_STREAM,0);
 	if(sockfd < 0){
 		error("ERROR opening socket");
-
 	}
+	server = gethostbyname(argv[1]);
+
+	if(server == NULL){
+		fprintf(stderr, "Error, nosuch host");
+	}
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *) server->h_addr , (char *) &serv_addr.sin_addr.s_addr, server->h_length);
+
 
 
 }
