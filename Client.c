a050989b9363c44c03c06a3a84940e63e6b845ee
wayared <jarecast@espol.edu.ec>
2023-04-23 20:27:23 -0700

    Instructions and libs

--- Client.c
+++ Client.c
@@ -1,2 +1,17 @@
-//
-//
+/*
+ * filename server_ipaddress portno
+ * argv[0] filename
+ * argv[1] server_ipaddress
+ * argv[2] portno
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+
