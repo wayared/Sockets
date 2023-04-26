36be604b13548ca6db2fc532ab34124c51c6a2fe
wayared <jarecast@espol.edu.ec>
2023-04-26 12:49:39 -0700

    Server.c file

--- Server.c
+++ Server.c
@@ -41,5 +41,37 @@ int main(int argc, char *argv[]){
 
 	if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
 		error("Binding Failed.");
+	listen(sockfd,5);
+	clilen = sizeof(cli_addr);
+
+	newsockfd = accept(sockfd ,(struct sockaddr *) &cli_addr, &clilen);
+
+	if(newsockfd < 0)
+		error("Error on Accept");
+	while(1){
+		bzero(buffer, 255);
+		n = read(newsockfd, buffer, 255);
+
+		if(n<0){
+		error("Error on reading");
+		}
+		printf("Client: %s\n", buffer);
+		bzero(buffer, 255);
+		fgets(buffer, 255, stdin);
+
+		n = write(newsockfd, buffer, strlen(buffer));
+
+		if(n < 0)
+			error("Error on Writting");
+
+		int i = strncmp("Bye", buffer, 3);
+
+		if(i == 0)
+			break;
+	}
+
+	close(newsockfd);
+	close(sockfd);
+	return 0;
 
 }
